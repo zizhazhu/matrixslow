f8290f364d2fc7bba2c7c49dcf0be138571acc92
zizhazhu <zizhazhu@gmail.com>
2022-10-25 19:18:07 +0800

    split trainer method

--- matrixslow/train/trainer.py
+++ matrixslow/train/trainer.py
@@ -1,6 +1,7 @@
+import abc
+
 import numpy as np
 from tqdm import tqdm
-from sklearn.preprocessing import OneHotEncoder
 
 
 class Trainer:
@@ -13,26 +14,25 @@ class Trainer:
         self.batch_size = batch_size
         self._is_multi_class = is_multi_class
 
-    def train_and_test(self, features, labels, test_features=None, test_labels=None, n_epochs=10, one_hot=False):
-        if one_hot is True:
-            one_hot_encoder = OneHotEncoder(sparse=False)
-            train_label = one_hot_encoder.fit_transform(labels.reshape(-1, 1))
-        else:
-            train_label = labels
+    def train_and_test(self, train_x, train_y, test_x=None, test_y=None, n_epochs=10):
+        self._variable_weights_init()
+        self.main_loop(train_x, train_y, test_x, test_y, n_epochs)
 
-        if test_features is None or test_labels is None:
-            test_features = features
-            test_labels = train_label
+    def main_loop(self, train_x, train_y, test_x=None, test_y=None, n_epochs=10):
+
+        if test_x is None or test_y is None:
+            test_x = train_x
+            test_y = train_y
 
         for epoch in range(n_epochs):
             batch_count = 0
-            for i in tqdm(range(len(features))):
+            for i in tqdm(range(len(train_x))):
                 if isinstance(self.x, list):
                     for j, x in enumerate(self.x):
-                        feature = np.mat(features[i][j]).T
+                        feature = np.mat(train_x[i][j]).T
                         x.set_value(feature)
                 else:
-                    feature = np.mat(features[i]).T
+                    feature = np.mat(train_x[i]).T
                     self.x.set_value(feature.reshape(self.x.dim))
 
                 label = np.mat(train_label[i]).T
@@ -46,13 +46,13 @@ class Trainer:
                     batch_count = 0
 
             pred = []
-            for i in range(len(test_features)):
+            for i in range(len(test_x)):
                 if isinstance(self.x, list):
                     for j, x in enumerate(self.x):
-                        feature = np.mat(test_features[i][j]).T
+                        feature = np.mat(test_x[i][j]).T
                         x.set_value(feature)
                 else:
-                    feature = np.mat(test_features[i]).T
+                    feature = np.mat(test_x[i]).T
                     self.x.set_value(feature.reshape(self.x.dim))
                 self.predict.forward()
                 pred.append(self.predict.value.A.ravel())
@@ -62,5 +62,26 @@ class Trainer:
             else:
                 pred = (np.array(pred) > 0.5).astype(int) * 2 - 1
                 pred = np.squeeze(pred)
-            acc = (test_labels == pred).astype(int).sum() / len(test_features)
+            acc = (test_y == pred).astype(int).sum() / len(test_x)
             print(f"Epoch: {epoch}, accuracy: {acc:.3f}")
+
+    def train(self):
+        pass
+
+    def eval(self):
+        pass
+
+    def _get_input_values(self, x):
+        pass
+
+    def one_step(self):
+        pass
+
+    @abc.abstractmethod
+    def _variable_weights_init(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def _optimizer_update(self):
+        raise NotImplementedError()
+
