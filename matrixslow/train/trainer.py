9f98818b82250876ec6599c949815f0fb43362c6
zizhazhu <zizhazhu@gmail.com>
2022-10-26 17:34:31 +0800

    update trainer

--- matrixslow/train/trainer.py
+++ matrixslow/train/trainer.py
@@ -6,13 +6,13 @@ from tqdm import tqdm
 
 class Trainer:
 
-    def __init__(self, x, y, predict, optimizer, batch_size=64, is_multi_class=False):
+    def __init__(self, x, y, predict, optimizer, batch_size=64, metric_ops=None):
         self.optimizer = optimizer
         self.x = x
         self.y = y
         self.predict = predict
         self.batch_size = batch_size
-        self._is_multi_class = is_multi_class
+        self.metric_ops = metric_ops
 
     def train_and_test(self, train_x, train_y, test_x=None, test_y=None, n_epochs=10):
         self._variable_weights_init()
@@ -20,62 +20,45 @@ class Trainer:
 
     def main_loop(self, train_x, train_y, test_x=None, test_y=None, n_epochs=10):
 
-        if test_x is None or test_y is None:
-            test_x = train_x
-            test_y = train_y
-
         for epoch in range(n_epochs):
-            batch_count = 0
-            for i in tqdm(range(len(train_x))):
-                if isinstance(self.x, list):
-                    for j, x in enumerate(self.x):
-                        feature = np.mat(train_x[i][j]).T
-                        x.set_value(feature)
-                else:
-                    feature = np.mat(train_x[i]).T
-                    self.x.set_value(feature.reshape(self.x.dim))
+            self.train(train_x, train_y)
+            if test_x is not None:
+                self.eval(test_x, test_y)
 
-                label = np.mat(train_label[i]).T
-                self.y.set_value(label)
+    def train(self, train_x, train_y):
+        for i in tqdm(range(len(train_x.values()[0]))):
+            self.one_step(self._get_input_values(train_x, i), train_y[i], is_training=True)
 
-                self.optimizer.one_step()
-                batch_count += 1
+            if (i + 1) % self.batch_size == 0:
+                self._optimizer_update()
 
-                if batch_count >= self.batch_size:
-                    self.optimizer.update()
-                    batch_count = 0
+    def eval(self, test_x, test_y):
+        for metric_op in self.metric_ops:
+            metric_op.reset()
+        for i in tqdm(range(len(test_x.values()[0]))):
+            self.one_step(self._get_input_values(test_x, i), test_y[i])
 
-            pred = []
-            for i in range(len(test_x)):
-                if isinstance(self.x, list):
-                    for j, x in enumerate(self.x):
-                        feature = np.mat(test_x[i][j]).T
-                        x.set_value(feature)
-                else:
-                    feature = np.mat(test_x[i]).T
-                    self.x.set_value(feature.reshape(self.x.dim))
-                self.predict.forward()
-                pred.append(self.predict.value.A.ravel())
+            for metric_op in self.metric_ops:
+                metric_op.forward()
 
-            if self._is_multi_class:
-                pred = np.array(pred).argmax(axis=1)
-            else:
-                pred = (np.array(pred) > 0.5).astype(int) * 2 - 1
-                pred = np.squeeze(pred)
-            acc = (test_y == pred).astype(int).sum() / len(test_x)
-            print(f"Epoch: {epoch}, accuracy: {acc:.3f}")
+        metrics_str = 'Evaluation metrics '
+        for metric_op in self.metric_ops:
+            metrics_str += metric_op.value_str()
+        print(metrics_str)
 
-    def train(self):
-        pass
+    def _get_input_values(self, x, index):
+        input_values = dict()
+        for node_name in x.keys():
+            input_values[node_name] = x[node_name][index]
+        return input_values
 
-    def eval(self):
-        pass
-
-    def _get_input_values(self, x):
-        pass
-
-    def one_step(self):
-        pass
+    def one_step(self, data_x, data_y, is_training=False):
+        for i in range(len(self.x)):
+            input_value = data_x.get(self.x[i].name)
+            self.x[i].set_value(np.mat(input_value).T)
+        self.y.set_value(np.mat(data_y).T)
+        if is_training:
+            self.optimizer.one_step()
 
     @abc.abstractmethod
     def _variable_weights_init(self):
